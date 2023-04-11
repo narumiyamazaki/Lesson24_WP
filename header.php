cd64f1063be49321ea00abb3c8db7b425fa5d7a3
naruiyamazaki <flaming.june123@gmail.com>
2023-04-11 17:26:23 +0900

    20230411_1725

--- header.php
+++ header.php
@@ -6,32 +6,54 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name=”description” content=”ハンドメイド猫グッズのお店ねこのもの” />
     <!-- CSS -->
-        <link rel="stylesheet" href="https://unpkg.com/destyle.css@1.0.5/destyle.css">
-        <link href="./css/style.css" rel="stylesheet">
-    <!--jQuery-->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <!-- Font Awesome -->
-        <script src="https://kit.fontawesome.com/b8a7fea4d4.js"></script>
-    <!--google fonts-->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
     </head>
 	<body>
-        <!--topに戻るボタン-->
-        <header class="l-header__lower-page">
-            <h1 class="p-heading1">
-                <img class="c-logo--top" src="./img/logo_top_header.svg" alt="ハンドメイド猫グッズのお店 ねこのもの l'atelier Queue">
-            </h1>
-            <div class="p-header__nav__wrapper--pc">
-                <nav class="p-header__nav__body--pc">
-                    <ul class="p-header__nav__items--pc">
-                        <li class="p-nav__item"><a class="c-item__link--nav" href="#">商品一覧</a></li>
-                        <li class="p-nav__item"><a class="c-item__link--nav" href="#">ねこブログ</a></li>
-                        <li class="p-nav__item"><a class="c-item__link--nav" href="#">ねこ店員紹介</a></li>
-                        <li class="p-nav__item"><a class="c-item__link--nav" href="#">よくあるご質問</a></li>
-                        <li class="p-nav__item"><a class="c-item__link--nav" href="#">お問い合わせ</a></li>
-                    </ul>
-                </nav>
+    <!--topに戻るボタン-->
+    <header class="l-header">
+        <div class="p-header__top__container">
+            <div class="p-header__top__wrapper">
+                <h1 class="p-heading1">
+                    <img class="c-logo--top" src="./img/logo_top_header.svg" alt="ハンドメイド猫グッズのお店 ねこのもの l'atelier Queue">
+                </h1>
+                <p class="c-text--top-explanation">“Queue(クー)”は、フランス語で「しっぽ」の意味です。<br>猫にも人にも心地好いをモットーに、猫おもちゃ、雑貨を作っています。</p>
             </div>
-        </header>
+            <div class="c-img--mv"></div>
+            <p class="p-nav__btn">
+                <a href="javascript:void(0)">
+                    <span>メニューを開く</span>
+                </a>
+                <span class="c-btn__text active">menu</span>
+                <span class="c-btn__text">close</span>
+            </p>
+        </div>
+        <!--gnav-->
+        <div class="p-header__nav__wrapper--sp">
+            <div class="c-logo__header__nav--sp u-margin-bottom--35px"></div>   
+            <nav class="p-header__nav__body--sp">
+                <ul class="p-header__nav__items--sp">
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">トップページ</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">商品一覧</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">ねこブログ</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">ねこ店員紹介</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">よくあるご質問</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">お問い合わせ</a></li>
+                </ul>
+                <ul class="p-header__nav__items--second">
+                    <li class="p-nav__item--second"><a class="c-item__link--second-nav" href="./">特定商取引法に関する表記</a></li>
+                    <span class="c-line__nav"></span>
+                    <li class="p-nav__item--second"><a class="c-item__link--second-nav" href="./">プライバシーポリシー</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="p-header__nav__wrapper--pc">
+            <nav class="p-header__nav__body--pc">
+                <ul class="p-header__nav__items--pc">
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">商品一覧</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">ねこブログ</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">ねこ店員紹介</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">よくあるご質問</a></li>
+                    <li class="p-nav__item"><a class="c-item__link--nav" href="#">お問い合わせ</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
